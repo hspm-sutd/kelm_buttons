e515327d8563e765df963ceb843532b0dbd60afc
polinakelm <73700777+polinakelm@users.noreply.github.com>
2020-11-26 15:29:21 +0300

    Add files via upload

---
+++ JS.js
@@ -0,0 +1,21 @@
+﻿let sum = 0;
+parseInt(sum);
+let num = prompt('Введите числа через пробел');
+let arr = num.split(' ');
+
+for (let i = 0; i < arr.length; i++) {
+    sum = sum + parseInt(arr[i]);
+}
+alert('Сумма = ' + (sum));
+
+function Click1() {
+    document.body.style.fontFamily = 'Times New Roman';
+    document.body.style.color = "blue";
+};
+
+function Click2() {
+    document.getElementById('div2').style.background = "linear-gradient(LightCoral, Red)";
+    document.getElementById('text2').style.background = "white";
+    document.getElementById('text2').style.mixBlendMode = "screen";
+    document.getElementById('text2').style.color = "black";
+};
